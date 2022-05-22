ec200620f5bcf4a1b557ed0f2bf9348b4e256171
monir3430 <miamonir3430@gmail.com>
2022-05-23 02:02:35 +0600

    post data

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
 const app = express()
@@ -6,6 +7,38 @@ const port = process.env.PORT || 5000
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.57qhu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+      await client.connect();
+      const productCollection = client.db('equipments').collection('tools');
+
+      app.get('/tools', async(req, res)=>{
+        const query = {};
+        const cursor = productCollection.find(query);
+        const tools = await cursor.toArray();
+        res.send(tools);
+      })
+
+       // post data to MongoDB ------------------------------
+
+       app.post('/tools', async(req, res)=>{
+        console.log(req.body)
+        const addTool = req.body;
+        const result =await productCollection.insertOne(addTool)
+        res.send(result);
+    })
+    }
+    finally{
+
+    }
+
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Equipment Personal')
